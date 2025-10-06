0942ac81eb6103250b422cdc8303f05870d9e926
Daria-Petrova <o.mine.God@gmail.com>
2025-10-06 23:22:15 +0300

    Добавлен слайдер, открытие и закрытие мобильного хедера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,36 @@
-/* в этот файл добавляет скрипты*/
+const slider = document.querySelector('#slider');
+const minElement = document.getElementById('min-price');
+const maxElement = document.getElementById('max-price');
+const toggleButton = document.querySelector('.toggle__button');
+const btn = document.querySelector('.toggle__button');
+const menu = document.querySelector('#toggle-menu');
+
+const formatForSlider = {
+  from: function (formattedValue) {
+    return Number(formattedValue);
+  },
+  to: function (numericValue) {
+    return Math.round(numericValue);
+  },
+};
+
+noUiSlider.create(slider, {
+  start: [0, 900],
+  connect: true,
+  range: {
+    min: 0,
+    max: 1000,
+  },
+  format: formatForSlider,
+});
+
+const formatValues = [minElement, maxElement];
+
+slider.noUiSlider.on('update', (values, handle) => {
+  formatValues[handle].value = values[handle];
+});
+
+toggleButton.addEventListener('click', () => {
+  btn.classList.toggle('toggle--open');
+  menu.classList.toggle('navigation-list--closed');
+});
